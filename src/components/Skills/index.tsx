65b1a795bc584cbc25458bfb7c543f6fa56e1ac2
Mohamed badry <85965151+mohamed44g@users.noreply.github.com>
2025-01-26 19:37:22 +0200

    Update index.tsx

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -17,6 +17,10 @@ export default function Index() {
           <div className="skills-box">CSS</div>
           <div className="skills-box">JavaScript</div>
           <div className="skills-box">React</div>
+          <div className="skills-box">Bootstrap</div>
+          <div className="skills-box">typescript</div>
+          <div className="skills-box">Sass</div>
+          <div className="skills-box">material ui</div>
           <div className="skills-box">React native</div>
           <div className="skills-box">Node.js</div>
           <div className="skills-box">Express</div>
