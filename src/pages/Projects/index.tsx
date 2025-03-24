b0fa86bcde238b2a1e78c6846ae0b90cdab50059
Mohamed badry <85965151+mohamed44g@users.noreply.github.com>
2025-03-24 22:44:20 +0200

    Update index.tsx

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,7 +1,7 @@
 import Title from "../../components/Title";
 import "./style.scss";
 import img1 from "../../assets/images/Screenshot 2024-08-22 105109.png";
-import img2 from "../../assets/images/Screenshot 2024-08-22 095937.png";
+import img2 from "../../assets/images/screenshot-1741016850908.jpeg";
 import img3 from "../../assets/images/Screenshot 2024-08-22 100121.png";
 import img4 from "../../assets/images/screenshot-1737907325535.jpeg";
 import ProjectCard from "../../components/projectCard";
@@ -12,6 +12,15 @@ export default function index() {
       <Title title="Projects" />
       <div className="container">
         <div className="d-flex flex-wrap cards justify-content-center align-items">
+          <ProjectCard
+            title="Physics Legends"
+            description=" An full stack e-learning website for Physics offers courses, online videos, and quizzes. Students can subscribe to any
+ course, track their progress, and post questions on the community page. Teachers can respond to these questions and
+ either accept or reject the posts. Additionally, students can privately chat with the teacher through the chat section."
+            img={img2}
+            preview="https://mohamed-seliim.com/"
+            codePreview="#"
+          />
           <ProjectCard
             title="Tripcraft"
             description="Travel agency for travel services and booking tickets online"
@@ -25,14 +34,6 @@ export default function index() {
             img={img1}
             preview="https://barca-store.vercel.app/"
             codePreview="https://github.com/mohamed44g/barca-store"
-          />
-
-          <ProjectCard
-            title="Game store"
-            description="Store for sailing video games and get games latest news."
-            img={img2}
-            preview="https://mohamed44g.github.io/Gaming/"
-            codePreview="https://github.com/mohamed44g/Gaming"
           />
 
           <ProjectCard
