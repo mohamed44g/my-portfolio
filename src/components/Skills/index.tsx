cc440b63283ee5ccf2a76ef2b97eab71dd34ae06
Mohamed badry <85965151+mohamed44g@users.noreply.github.com>
2025-07-03 02:11:51 +0300

    Update index.tsx

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -18,13 +18,12 @@ export default function Index() {
           <div className="skills-box">JavaScript</div>
           <div className="skills-box">React</div>
           <div className="skills-box">Bootstrap</div>
-          <div className="skills-box">WordPress</div>
           <div className="skills-box">typescript</div>
-          <div className="skills-box">Sass</div>
           <div className="skills-box">material ui</div>
           <div className="skills-box">Node.js</div>
+          <div className="skills-box">nest js</div>
           <div className="skills-box">Express</div>
-          <div className="skills-box">MongoDb</div>
+          <div className="skills-box">Postgres sql</div>
         </div>
       </div>
     </>
